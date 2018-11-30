b2a3b81a355e044f3ba25c761b684b9ca25ff9f2
Fergus Lemon <fergus.lemon@gmail.com>
2018-11-30 18:01:27 +0000

    Require the Bookmark class, route to the homepage view and add a before block.

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra/base'
+require File.join(File.dirname(__FILE__), 'lib', 'bookmark')
 
 class Bookmarker < Sinatra::Base
 
+  before do
+    @bookmark_list = Bookmark.all
+  end
+
   get '/' do
+    erb :index
   end
 end
