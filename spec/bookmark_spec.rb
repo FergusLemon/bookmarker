42169b718ee9eecf660091662cdcb0022fdeb4d7
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-02 14:00:40 +0000

    Create unit test for ::all when a bookmark has been added.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -9,5 +9,11 @@ describe Bookmark do
         expect(described_class.all).to eq('No bookmarks have been added yet.')
       end
     end
+    context 'when one bookmark has been added' do
+      it 'should return the bookmark' do
+        bookmark
+        expect(described_class.all).to include(bookmark)
+      end
+    end
   end
 end
