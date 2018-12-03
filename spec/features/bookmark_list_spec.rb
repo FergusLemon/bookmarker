35f5bee062b873a4f9071d1334625d3e0eeaf34d
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-03 10:30:40 +0000

    Refactor to make feature tests more robust.

--- spec/features/bookmark_list_spec.rb
+++ spec/features/bookmark_list_spec.rb
@@ -1,11 +1,11 @@
 feature 'Getting a List of All Bookmarks' do
   scenario 'When no bookmarks have been added' do
-    allow(Bookmark.all).to receive(:empty?).and_return(true)
+    allow(Bookmark).to receive(:all).and_return('No bookmarks have been added yet.')
     visit '/'
     should_see 'No bookmarks have been added yet.'
   end
   scenario 'When bookmarks have been added' do
-    allow(Bookmark.all).to receive(:empty?).and_return(false)
+    allow(Bookmark).to receive(:all).and_return('https://github.com')
     visit '/'
     should_not_see 'No bookmarks have been added yet.'
   end
