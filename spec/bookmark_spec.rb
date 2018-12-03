8bd7539391fd6082e6326f1101f16c4524f47646
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-03 09:04:35 +0000

    Refactor - use correct method name.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,7 @@ require 'bookmark'
 describe Bookmark do
   let(:bookmark) { described_class.new }
 
-  describe '::all_bookmarks' do
+  describe '::all' do
     context 'when no bookmarks have been added' do
       it 'should return a message to the user' do
         expect(described_class.all).to eq('No bookmarks have been added yet.')
