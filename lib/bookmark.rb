21971ba89a24c19cac0fbebdb22d0ea162517aad
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-04 08:51:10 +0000

    Update ::retrieve_bookmarks to run on test DB for specs and normal DB when running locally.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -18,7 +18,11 @@ class Bookmark
 
   class << self
     def retrieve_bookmarks
-      conn = PG.connect(dbname: 'bookmarker')
+      if ENV['RACK_ENV'] == 'test'
+        conn = PG.connect(dbname: 'bookmarker_test')
+      else
+        conn = PG.connect(dbname: 'bookmarker')
+      end
       result = conn.exec('SELECT * FROM bookmarks')
       rows = result.map { |row| row.values_at('url') }.flatten
       rows.size.zero? ? 'No bookmarks have been added yet.' : rows
