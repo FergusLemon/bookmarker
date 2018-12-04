e6550e0eee938e2e1f2f7fe23ee0eddc04b3adeb
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-04 09:18:24 +0000

    Correct missing semi-colon.

--- spec/support/setup_test_database.rb
+++ spec/support/setup_test_database.rb
@@ -2,6 +2,6 @@ require 'pg'
 module DatabaseHelpers
   def truncate_table
    conn = PG.connect(dbname: 'bookmarker_test')
-   conn.exec("TRUNCATE bookmarks")
+   conn.exec("TRUNCATE bookmarks;")
   end
 end
