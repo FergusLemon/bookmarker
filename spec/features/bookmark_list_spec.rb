127f7f49dadda6d8e00d83d74b5d5ca87a79928f
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-04 12:39:32 +0000

    Refactor test to use new method behaviour in Bookmark rather than opening a connection to the DB.

--- spec/features/bookmark_list_spec.rb
+++ spec/features/bookmark_list_spec.rb
@@ -4,8 +4,7 @@ feature 'Viewing Bookmarks' do
     should_see 'No bookmarks have been added yet.'
   end
   scenario 'Sees bookmarks that have been added' do
-    conn = PG.connect(dbname: 'bookmarker_test')
-    conn.exec("INSERT INTO bookmarks(url) VALUES('https://github.com');")
+    Bookmark.new('https://github.com')
     visit '/bookmarks'
     should_not_see 'No bookmarks have been added yet.'
     should_see 'https://github.com'
