148a6013dcb760cbe9827e60b09c0ed1d85769ed
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-05 08:01:25 +0000

    Remove message prompt from view, update spec accordingly.

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -1,7 +1,6 @@
 feature 'Adding Bookmarks' do
   scenario 'Sees an option to add a bookmark' do
     visit '/bookmarks'
-    should_see 'If you would like to add a new bookmark, enter the URL below:'
     expect(page).to have_css('#enter-url')
     expect(page).to have_css('#add-bookmark')
   end
