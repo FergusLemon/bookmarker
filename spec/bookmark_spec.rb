ae4ffe70f8178c228997ffa773e77531ab1b8b6c
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-05 10:59:10 +0000

    Update spec to expect bookamrks to ahve a URL and title.

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,34 +1,33 @@
 require 'bookmark'
 
 describe Bookmark do
-  let(:bookmark) { described_class.new }
-  let(:db_bookmark) { 'https://reddit.com' }
-  let(:conn) { PG.connect(dbname: 'bookmarker_test') }
+  let(:url) { 'https://reddit.com' }
+  let(:title) { 'Reddit Homepage' }
+  let(:bookmark) { described_class.new(url, title) }
+  let(:connection) { PG.connect(dbname: 'bookmarker_test') }
   let(:bookmarks) { described_class.all }
 
   describe '::all' do
     context 'when no bookmarks have been added' do
-      it 'should return a message to the user' do
-        allow(described_class).to receive(:retrieve_bookmarks).and_return('No bookmarks have been added yet.')
-        expect(bookmarks).to eq('No bookmarks have been added yet.')
+      it 'should be empty' do
+        expect(bookmarks).to be_empty
       end
       it 'should not return a bookmark' do
-        allow(described_class).to receive(:retrieve_bookmarks).and_return('No bookmarks have been added yet.')
-        expect(bookmarks).not_to include(db_bookmark)
+        expect(bookmarks).not_to include(url)
       end
     end
     context 'when one bookmark has been added' do
       it 'should return the bookmark' do
-        conn.exec("INSERT INTO bookmarks(url) VALUES('https://reddit.com');")
-        expect(bookmarks).to include(db_bookmark)
+        connection.exec("INSERT INTO bookmarks(url, title) VALUES('https://reddit.com', 'Reddit Homepage');")
+        expect(bookmarks).to include(url)
       end
     end
   end
 
   describe '#initialize' do
     it 'adds a new bookmark to the bookmark list' do
-      described_class.new(db_bookmark)
-      expect(bookmarks).to include(db_bookmark)
+      described_class.new('test_url', 'test_title')
+      expect(bookmarks).to include('test_url')
     end
   end
 end
