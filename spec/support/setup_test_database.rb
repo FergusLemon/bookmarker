0b71b1e0d3d869df2f4b5c0953487280005e94ca
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-08 15:45:11 +0000

    Update to Truncate both the bookmarks and comments table before each test.

--- spec/support/setup_test_database.rb
+++ spec/support/setup_test_database.rb
@@ -2,6 +2,7 @@ require 'pg'
 module DatabaseHelpers
   def truncate_table
    conn = PG.connect(dbname: 'bookmarker_test')
-   conn.exec("TRUNCATE bookmarks RESTART IDENTITY;")
+   conn.exec("TRUNCATE bookmarks RESTART IDENTITY CASCADE;")
+   conn.exec("TRUNCATE comments RESTART IDENTITY CASCADE;")
   end
 end
