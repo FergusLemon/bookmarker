caf0a07783975b8cdd60bef31d60aaa578f64d6f
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-09 11:09:08 +0000

    Create spec for and implement ::where.

--- lib/comment.rb
+++ lib/comment.rb
@@ -3,8 +3,16 @@ attr_reader :id, :text, :bookmark_id
 
   class << self
     def create(comment, bookmark_id)
-      result = DatabaseConnection.query("INSERT INTO comments(text, bookmark_id) VALUES('#{comment}', '#{bookmark_id}') RETURNING id, text, bookmark_id;")
-      self.new(id: result[0]['id'], text: result[0]['text'], bookmark_id: result[0]['bookmark_id'])
+      result = DatabaseConnection.query("INSERT INTO comments(text, bookmark_id)\
+                                        VALUES('#{comment}', '#{bookmark_id}')\
+                                        RETURNING id, text, bookmark_id;")
+      wrap_database_results(result).pop
+    end
+
+    def where(bookmark_id)
+      result = DatabaseConnection.query("SELECT * FROM comments WHERE\
+                                         bookmark_id='#{bookmark_id}';")
+      wrap_database_results(result)
     end
   end
 
@@ -12,5 +20,15 @@ attr_reader :id, :text, :bookmark_id
     @id = args[:id]
     @text = args[:text]
     @bookmark_id = args[:bookmark_id]
+  end
+
+  private
+
+  class << self
+    def wrap_database_results(table_data)
+      table_data.map do |row|
+        self.new(id: row['id'], text: row['text'], bookmark_id: row['bookmark_id'])
+      end
+    end
   end
 end
--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,6 +5,7 @@ require_relative 'support/database_helpers'
 describe Comment do
   let(:comment_class) { described_class }
   let(:text) { 'Test comment' }
+  let(:more_text) { 'Another Test comment' }
   let(:url) { 'https://github.com' }
   let(:title) { 'Github Homepage' }
 
@@ -16,6 +17,20 @@ describe Comment do
       comments = persisted_data(table: 'comments', id: comment.id)
       expect(comments.first['bookmark_id']).to eq(bookmark_id)
       expect(comments.first['text']).to eq(text)
+    end
+  end
+
+  describe '::where' do
+    it 'returns all comments made on a bookmark' do
+      Bookmark.create(url, title)
+      bookmark_id = Bookmark.all.to_a.last.id
+      test_comment = comment_class.create(text, bookmark_id)
+      another_test_comment = comment_class.create(more_text, bookmark_id)
+      comments = comment_class.where(bookmark_id)
+      expect(comments).not_to be_empty
+      expect(comments.size).to eq(2)
+      expect(comments.first.text).to eq(text)
+      expect(comments.last.text).to eq(more_text)
     end
   end
 end
