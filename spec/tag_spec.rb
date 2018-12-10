ede7d569b12ff952b67b86822b824eef19172140
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-10 10:27:31 +0000

    Write unit test on ::create to check that an entry is made into the join table with the correct bookmark id and use let statements for common methods.

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -7,14 +7,18 @@ describe Tag do
   let(:url) { 'https://github.com' }
   let(:title) { 'Github Homepage' }
   let(:content) { 'Code' }
+  let!(:bookmark) { Bookmark.create(url, title) }
+  let!(:bookmark_id) { Bookmark.all.to_a.last.id }
+  let!(:tag) { tag_class.create(content, bookmark_id) }
+  let(:tags) { persisted_data(table: 'tags', id: tag.id) }
+  let(:join) { join_data(table: 'bookmark_tags', tag_id: tag.id) }
 
   describe '::create' do
     it 'adds a tag to a bookmark' do
-      Bookmark.create(url, title)
-      bookmark_id = Bookmark.all.to_a.last.id
-      tag = tag_class.create(content, bookmark_id)
-      tags = persisted_data(table: 'tags', id: tag.id)
       expect(tags.first['content']).to eq(content)
+    end
+    it 'adds an entry in the join table' do
+      expect(join.first['bookmark_id']).to eq(bookmark_id)
     end
   end
 end
