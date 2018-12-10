de24571301d7c39defb8746dff3ee3ac72609ec5
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-10 10:35:28 +0000

    Refactor to DRY out code with let statements.

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -8,13 +8,13 @@ describe Comment do
   let(:more_text) { 'Another Test comment' }
   let(:url) { 'https://github.com' }
   let(:title) { 'Github Homepage' }
+  let!(:bookmark) { Bookmark.create(url, title) }
+  let!(:bookmark_id) { Bookmark.all.to_a.last.id }
+  let!(:comment) { comment_class.create(text, bookmark_id) }
+  let(:comments) { persisted_data(table: 'comments', id: comment.id) }
 
   describe '::create' do
     it 'should add a comment to the comments table' do
-      Bookmark.create(url, title)
-      bookmark_id = Bookmark.all.to_a.last.id
-      comment = comment_class.create(text, bookmark_id)
-      comments = persisted_data(table: 'comments', id: comment.id)
       expect(comments.first['bookmark_id']).to eq(bookmark_id)
       expect(comments.first['text']).to eq(text)
     end
@@ -22,12 +22,9 @@ describe Comment do
 
   describe '::where' do
     it 'returns all comments made on a bookmark' do
-      Bookmark.create(url, title)
-      bookmark_id = Bookmark.all.to_a.last.id
-      comment_class.create(text, bookmark_id)
       comment_class.create(more_text, bookmark_id)
       comments = comment_class.where(bookmark_id)
-      expect(comments.size).to eq(2)
+      expect(comments.length).to eq(2)
       expect(comments.first.text).to eq(text)
       expect(comments.last.text).to eq(more_text)
     end
