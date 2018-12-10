bbacf23f383b8a75ffaa13014b0e628828533af8
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-10 10:26:32 +0000

    Add helper to retrieve data from the bookmark_tag join table.

--- spec/support/database_helpers.rb
+++ spec/support/database_helpers.rb
@@ -3,3 +3,7 @@ require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'database_connectio
 def persisted_data(table:, id:)
   DatabaseConnection.query("SELECT * FROM #{table} WHERE id = #{id};")
 end
+
+def join_data(table:, tag_id:)
+  DatabaseConnection.query("SELECT * FROM #{table} WHERE tag_id = #{tag_id};")
+end
