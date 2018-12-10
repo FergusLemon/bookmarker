fb48c7ebe15cac68c3013555604752e05d498ff6
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-10 08:03:49 +0000

    Add feature test for viewing all comments.

--- spec/features/adding_comments_spec.rb
+++ spec/features/adding_comments_spec.rb
@@ -1,10 +1,17 @@
- feature 'Adding comments' do
-   scenario 'User adds a comment to a bookmark' do
+ feature 'Adding and viewing comments' do
+   scenario 'A user can add a comment to a bookmark' do
     create_bookmark
-    click_on 'add-comment'
-    fill_in('comment', :with => 'This is a test comment')
-    click_on 'submit-comment'
+    create_comment('This is a test comment')
     click_on 'comments'
     should_see 'This is a test comment'
+   end
+
+   scenario 'A user can view all comments on a bookmark' do
+    create_bookmark
+    create_comment('This is a test comment')
+    create_comment('This is another test comment')
+    click_on 'comments'
+    should_see 'This is a test comment'
+    should_see 'This is another test comment'
    end
  end
