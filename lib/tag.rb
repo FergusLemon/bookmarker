dc47b67ea30415aad4bb93655a8537df41915078
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-10 12:24:22 +0000

    Implement ::where.

--- lib/tag.rb
+++ lib/tag.rb
@@ -10,6 +10,16 @@ attr_reader :id, :content
                                VALUES('#{bookmark_id}', '#{result[0]['id']}');")
       wrap_database_results(result).pop
     end
+
+    def where(bookmark_id)
+      join_data = DatabaseConnection.query("SELECT * FROM bookmark_tags WHERE\
+                                           bookmark_id='#{bookmark_id}';")
+      tag_ids = join_data.map { |row| row['tag_id'] }
+      result = tag_ids.map do |id|
+        DatabaseConnection.query("SELECT * FROM tags WHERE id='#{id}';")
+      end
+      wrap_join_results(result)
+    end
   end
 
   def initialize(**args)
@@ -21,9 +31,16 @@ attr_reader :id, :content
 
   class << self
     def wrap_database_results(table_data)
-      table_data.map do |row|
-        self.new(id: row['id'],\
+        table_data.map do |row|
+          self.new(id: row['id'],\
                  content: row['content'])
+      end
+    end
+
+    def wrap_join_results(table_data)
+        table_data.map do |row|
+          self.new(id: row[0]['id'],\
+                 content: row[0]['content'])
       end
     end
   end
