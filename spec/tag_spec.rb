92e9553145d1a5b8a4189315a2b1498e2a603620
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-11 17:04:27 +0000

    Write unit test for ::all and to check that duplicate tags are not entered under different ids as well as remove superfluous test for behaviour extracted to the BookmarkTag class.

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,26 +1,50 @@
 require 'tag'
 require 'bookmark'
+require 'bookmark_tag'
 require_relative 'support/database_helpers'
 
 describe Tag do
   let(:tag_class) { described_class }
+  let(:bookmark_class) { double('Bookmark Clas') }
   let(:url) { 'https://github.com' }
   let(:title) { 'Github Homepage' }
+  let(:other_url) { 'https://reddit.com' }
+  let(:other_title) { 'Reddit Homepage' }
   let(:content) { 'Code' }
   let(:other_content) { 'Repository' }
   let!(:bookmark) { Bookmark.create(url, title) }
-  let!(:bookmark_id) { Bookmark.all.to_a.last.id }
+  let!(:bookmark_id) { Bookmark.all.to_a.first.id }
+  let!(:another_bookmark) { Bookmark.create(other_url, other_title) }
+  let!(:another_bookmark_id) { Bookmark.all.to_a.last.id }
   let!(:tag) { tag_class.create(content, bookmark_id) }
+  let!(:tag_id) { tag_class.all.to_a.last.id }
+  let!(:bookmark_tag) { BookmarkTag.create(bookmark_id: bookmark_id, tag_id: tag_id) }
   let!(:another_tag) { tag_class.create(other_content, bookmark_id) }
+  let!(:another_tag_id) { tag_class.all.to_a.last.id }
+  let!(:another_bookmark_tag) { BookmarkTag.create(bookmark_id: bookmark_id, tag_id: another_tag_id) }
   let(:tags) { persisted_data(table: 'tags', id: tag.id) }
-  let(:join) { join_data(table: 'bookmark_tags', tag_id: tag.id) }
 
   describe '::create' do
     it 'adds a tag to a bookmark' do
       expect(tags.first['content']).to eq(content)
     end
-    it 'adds an entry in the join table' do
-      expect(join.first['bookmark_id']).to eq(bookmark_id)
+    context 'where a tag already exists' do
+      it 'does not add a duplicate tag to the tags table' do
+        expect { tag }.not_to change { tags.to_a.length }
+        expect { tag_class.create(content, another_bookmark_id) }.not_to change\
+          { tags.to_a.length }
+      end
+    end
+  end
+
+  describe '::all' do
+    context 'when two tags exists' do
+      it 'should not be empty' do
+        expect(tag_class.all).not_to be_empty
+      end
+      it 'should include the tags' do
+        expect(tag_class.all.last.id).to eq(another_tag.id)
+      end
     end
   end
 
@@ -30,6 +54,14 @@ describe Tag do
       expect(tags.length).to eq(2)
       expect(tags.first.content).to eq(content)
       expect(tags.last.content).to eq(other_content)
+    end
+  end
+
+  describe '::bookmarks' do
+    it 'returns all bookmarks associated with the tag' do
+      tag_class.create(content, another_bookmark_id)
+      expect(bookmark_class).to receive(:where).with(tag_id: tag_id)
+      tag_class.bookmarks(bookmark_class, tag_id)
     end
   end
 end
