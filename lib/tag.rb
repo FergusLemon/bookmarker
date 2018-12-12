5c332befe82f232cc145d779f5daa7a80c3024ab
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-12 15:56:05 +0000

    Update ::create so that no new tag is created if one with the same content already exists, returns id of existing tag if there is one.

--- lib/tag.rb
+++ lib/tag.rb
@@ -3,22 +3,32 @@ attr_reader :id, :content
 
   class << self
     def create(content, bookmark_id)
-      result = DatabaseConnection.query("INSERT INTO tags(content)\
-                                        VALUES('#{content}')\
-                                        RETURNING id, content;")
-      DatabaseConnection.query("INSERT INTO bookmark_tags(bookmark_id, tag_id)\
-                               VALUES('#{bookmark_id}', '#{result[0]['id']}');")
-      wrap_database_results(result).pop
+      tag_exists = DatabaseConnection.query("SELECT id FROM tags WHERE\
+                                            tags.content = '#{content}';").first
+      if !tag_exists
+        result = DatabaseConnection.query("INSERT INTO tags(content)\
+                                          VALUES('#{content}')\
+                                          RETURNING id, content;")
+        wrap_database_results(result).pop
+      else
+        self.all.select { |tag| tag.id == tag_exists['id'] }.pop
+      end
     end
 
     def where(bookmark_id)
-      join_data = DatabaseConnection.query("SELECT * FROM bookmark_tags WHERE\
-                                           bookmark_id='#{bookmark_id}';")
-      tag_ids = join_data.map { |row| row['tag_id'] }
-      result = tag_ids.map do |id|
-        DatabaseConnection.query("SELECT * FROM tags WHERE id='#{id}';")
-      end
-      wrap_join_results(result)
+      result = DatabaseConnection.query("SELECT tags.id, content FROM tags\
+                                        INNER JOIN bookmark_tags ON\
+                                        tags.id = bookmark_tags.tag_id WHERE\
+                                        bookmark_tags.bookmark_id = '#{bookmark_id}';")
+      wrap_database_results(result)
+    end
+
+    def all
+      retrieve_tags
+    end
+
+    def bookmarks(bookmark_class = Bookmark, id)
+      retrieve_bookmarks(bookmark_class, id)
     end
   end
 
@@ -30,17 +40,19 @@ attr_reader :id, :content
   private
 
   class << self
+    def retrieve_tags
+      tags_data = DatabaseConnection.query('SELECT * FROM tags')
+      wrap_database_results(tags_data)
+    end
+
+    def retrieve_bookmarks(bookmark_class, id)
+      bookmark_class.where(tag_id: id)
+    end
+
     def wrap_database_results(table_data)
         table_data.map do |row|
           self.new(id: row['id'],\
                  content: row['content'])
-      end
-    end
-
-    def wrap_join_results(table_data)
-        table_data.map do |row|
-          self.new(id: row[0]['id'],\
-                 content: row[0]['content'])
       end
     end
   end
