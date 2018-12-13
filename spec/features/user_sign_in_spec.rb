9794d89cba227b1f61b27e91d51a6cf6fffa1740
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-13 21:10:44 +0000

    Add feature test for invalid sign ins.

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -8,4 +8,17 @@ feature 'User sign in' do
     click_on 'Submit'
     should_see 'Logged in as test@gmail.com'
   end
+  scenario 'If the wrong username or password is provided an error message is shown' do
+    User.create(username: 'test@gmail.com', password: 'myPa$$word')
+    visit '/'
+    click_on 'Sign in'
+    fill_in 'username', with: 'test11@gmail.com'
+    fill_in 'password', with: 'myPa$$word'
+    click_on 'Submit'
+    should_see 'The username or password entered were incorrect, please try again.'
+    fill_in 'username', with: 'test11@gmail.com'
+    fill_in 'password', with: 'myPa$$word'
+    click_on 'Submit'
+    should_see 'The username or password entered were incorrect, please try again.'
+  end
 end
